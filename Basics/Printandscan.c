089ed5265472efae01a5e00246420bd94d62d129
Akhil Nagipogu <167780359+AkhilNagipogu@users.noreply.github.com>
2025-12-07 20:19:00 +0530

    Add files via upload

---
+++ Basics/Printandscan.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{   
+    int a;
+    printf("Hello World\n");
+    printf("And the user who is running the code\n");
+    printf("Pick a number 1 or 2, I am trying to choose something: ");
+    scanf("%d", &a);
+    printf("You for real had to pick %d\n", a);
+    printf("You are a dumb dumb\n");
+    printf("Adios");
+    return 0;
+}
